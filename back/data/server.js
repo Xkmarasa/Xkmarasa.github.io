34b3a6546cba72dc15d9af9b6e5edd0a70800c03
Xavi <xavi2712@gmail.com>
2025-07-21 21:01:36 +0200

    sercer

--- back/data/server.js
+++ back/data/server.js
@@ -339,7 +339,7 @@ app.post('/usuarios', async (req, res) => {
       return res.status(401).json({ error: 'Usuario no encontrado' });
     }
 
-    const match = await bcrypt.compare(contrasena, user.contrasena_hash);
+    const match = await bcrypt.compare(contrasena, user.contrasena);
     if (!match) {
       return res.status(401).json({ error: 'Contraseña incorrecta' });
     }
